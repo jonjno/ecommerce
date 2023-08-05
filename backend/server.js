602a953b47d4ab59f4a7cf95d44824aba5d2fdeb
jonjno <sheeja4106@gmail.com>
2023-08-05 18:25:48 +0530

    first commit

--- backend/server.js
+++ backend/server.js
@@ -32,10 +32,10 @@ app.use("/api/users", userRouter);
 app.use("/api/orders", OrderRouter);
 
 const __dirname = path.resolve();
-console.log(__dirname);
-app.use(express.static(path.join(__dirname, "/frontend/build")));
+console.log("hai", __dirname);
+app.use(express.static(path.join(__dirname, "frontend", "build")));
 app.get("*", (req, res) =>
-  res.sendFile(path.join(__dirname, "/frontend/build/index.html"))
+  res.sendFile(path.join(__dirname, "./frontend/build/index.html"))
 );
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
